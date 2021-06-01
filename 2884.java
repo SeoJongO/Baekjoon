db43a4956278f417f4d96b588141d4a524db9844
서종오 <84363963+SeoJongO@users.noreply.github.com>
2021-06-01 21:43:38 +0900

    if문 단계

--- 2884.java
+++ 2884.java
@@ -7,14 +7,16 @@ public class Main {
 		int H = sc.nextInt();
 		int M = sc.nextInt();
 		sc.close();
-		if (45 < M) {
+
+		if (45 > M) {
+			H--;
+			M = 60 - (45 - M);
+			if (0 > H) {
+				H = 23;
+			}
+			System.out.println(H + " " + M);
+		} else {
 			System.out.println(H + " " + (M - 45));
-		} else if (45 > M) {
-			H -= 1;
-				if (0 > H) {
-					H = 23;
-				}
-			System.out.println(H + " " + (60 - (45 - M)));
 		}
 	}
 }
